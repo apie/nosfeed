bc9363890c105cb4db07ebb822246e5bd8486bb9
Denick <1049485+apie@users.noreply.github.com>
2023-08-13 11:15:05 +0200

    Move feeds and blacklist to files

---
+++ blacklist.js
@@ -0,0 +1,14 @@
+var blacklist = [
+    'FC Twente',
+    'Oranje',
+    'Rusland',
+    'Oekraïne',
+    'Oekraïnse',
+    'Rusland',
+    'Russische',
+    'droneaanval',
+    'tour de france',
+    'volleybal',
+    'WK-wedstrijd',
+];
+
